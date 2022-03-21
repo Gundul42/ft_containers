eef1ef61280ccefa94d4cd337936695a05ec2ced
Gundul <sampah@mailbox.org>
2022-03-21 15:55:05 +0100

    more

--- main.cpp
+++ main.cpp
@@ -6,17 +6,30 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 09:50:25 by graja             #+#    #+#             */
-/*   Updated: 2022/03/19 18:55:29 by graja            ###   ########.fr       */
+/*   Updated: 2022/03/21 15:45:45 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.hpp"
 #include <vector>
 
+static
+void	vprint(ft::vector<float> tmp)
+{
+	size_t	i = 0;
+
+	while (i < tmp.size())
+	{
+		std::cout << tmp[i] << ", ";
+		i++;
+	}
+	std::cout << std::endl;
+}
+
 int main(void)
 {
 	ft::vector<float>	a(2);
-	ft::vector<float>	d(a);
+	ft::vector<float>	d(0);
 	std::vector<float>	b(10);
 	std::vector<float>	c(20);
 	std::vector<float>::iterator	it;
@@ -26,10 +39,24 @@ int main(void)
 	std::cout << " -- empty = " << a.empty() << " -- max_size = " << a.max_size();
 	std::cout << std::endl;
 	a.push_back(100);
-	//d = a;
+	a.push_back(11);
+	a.push_back(-32511);
+	vprint(a);
+	d = a;
+	d[0] = -1213;
 	std::cout << "size = " << d.size() << " -- capacity = " << d.capacity();
 	std::cout << " -- empty = " << d.empty() << " -- max_size = " << d.max_size();
 	std::cout << std::endl;
+	vprint(d);
+	d.pop_back();
+	vprint(d);
+	std::cout << "size = " << d.size() << " -- capacity = " << d.capacity();
+	std::cout << " -- empty = " << d.empty() << " -- max_size = " << d.max_size();
+	std::cout << std::endl;
+	std::cout << "size = " << a.size() << " -- capacity = " << a.capacity();
+	std::cout << " -- empty = " << a.empty() << " -- max_size = " << a.max_size();
+	std::cout << std::endl;
+	std::cout << d.front() << " : " << d.back() <<  " : " << d.at(8) << std::endl;
 	/*
 	std::cout << "size = " << b.size() << " -- capacity = " << b.capacity();
 	std::cout << " -- empty = " << b.empty() << " -- max_size = " << b.max_size();
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 09:30:11 by graja             #+#    #+#             */
-/*   Updated: 2022/03/20 15:39:10 by graja            ###   ########.fr       */
+/*   Updated: 2022/03/21 15:54:23 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,29 @@ namespace ft
 			T	*_end_of_storage;
 			Alloc	_alloc;
 
+			typedef	std::size_t				size_type;
+			typedef std::ptrdiff_t				difference_type;
+
+			void	_realloc(size_type newsize, size_type newcapacity)
+			{
+				T		*_ns;
+				difference_type	i = 0;
+
+				_ns = _alloc.allocate(newcapacity);
+				while (i < _finish - _start)
+				{
+					_ns[i] = _start[i];
+					i++;
+				}
+				_alloc.deallocate(_start, _end_of_storage - _start);
+				_start = _ns;
+				_finish = _start + newsize;
+				_end_of_storage = _start + newcapacity;
+			}
+
 		public:
 			typedef	T					value_type;
 			typedef Alloc					allocator_type;
-			typedef	std::size_t				size_type;
-			typedef std::ptrdiff_t				difference_type;
 			typedef	value_type&				reference;
 			typedef	const value_type&			const_reference;
 			typedef typename Alloc::pointer			pointer;
@@ -48,7 +66,7 @@ namespace ft
 
 			vector(void): _start(_alloc.allocate(1)), _finish(_start),
 					_end_of_storage(_start) {}
-			vector(size_t n): _start(_alloc.allocate(n)), _finish(_start + n),
+			vector(size_type n): _start(_alloc.allocate(n)), _finish(_start + n),
 					_end_of_storage(_start + n) {}
 			vector(vector<T> const & cpy): _start(_alloc.allocate(cpy.capacity() + 1)),
 				       	_finish(_start + cpy.size()),
@@ -60,8 +78,7 @@ namespace ft
 				size_type	i = 0;
 				if (this->size() < right.size())
 				{
-					std::cout << "Realloc needed" << std::endl;
-					return (*this);
+					_realloc(right.size(), right.capacity());
 				}
 				while (i < right.size())
 				{
@@ -79,15 +96,46 @@ namespace ft
 			void		pop_back(void) {_finish--;}
 			void		push_back(T data)
 			{
-				if (this->capacity() > this->size())
-				{
-					*_finish = data;
-					_finish++;
-				}
-				else
-					std::cout << "Realloc needed!" << std::endl;
+				if (this->capacity() == this->size())
+					_realloc(this->size(), this->size() * 2);
+				*_finish = data;
+				_finish++;
+			}
+		
+
+			// Capacity member functions	
+			T &	operator[](size_type n)
+			{
+				return (*(this->_start + n));
+			}
+			
+			T const &	operator[](size_type n) const
+			{
+				return (*(this->_start + n));
 			}
 
+			T &	at(size_type n)
+			{
+				if (n < 0 || n > this->size())
+					throw std::out_of_range("Index out of range");
+				else
+					return (_start[n]);
+			}
+
+			T const &	at(size_type n) const
+			{
+				if (n < 0 || n > this->size())
+					throw std::out_of_range("Index out of range");
+				else
+					return (_start[n]);
+			}
+
+			T &		front(void) {return (*(this->_start));}
+			T const &	front(void) const  {return (*(this->_start));}
+
+			T &		back(void) {return ((_start[size() - 1]));}
+			T const &	back(void) const  {return ((this[size() - 1]));}
+			
 	};
 }
 
